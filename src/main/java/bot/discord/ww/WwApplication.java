97950cf45611196e959320f23c10bbd0062d09c7
sunwook1996 <hanyang9984@naver.com>
2022-02-14 00:26:54 +0900

    ubuntu 서버용 key path 지정

--- src/main/java/bot/discord/ww/WwApplication.java
+++ src/main/java/bot/discord/ww/WwApplication.java
@@ -4,12 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.io.File;
+
 @SpringBootApplication
 @EnableScheduling
 public class WwApplication {
 
 	public static void main(String[] args) {
+
+		if (args.length > 0) {
+			args[1].equals("server");
+			System.setProperty("keypath", File.separator + "home" + File.separator + "ubuntu" + File.separator + "key" + File.separator + "key.txt");
+		}
+
 		SpringApplication.run(WwApplication.class, args);
+
 	}
 
 }
--- src/main/java/bot/discord/ww/common/ApplicationInit.java
+++ src/main/java/bot/discord/ww/common/ApplicationInit.java
@@ -21,7 +21,9 @@ public class ApplicationInit {
     @PostConstruct
     public void init() throws Exception {
 
-        Path fileName = Path.of("C:\\Key\\key.txt");
+        String keypath = System.getProperty("keypath", "C:\\Key\\key.txt");
+
+        Path fileName = Path.of(keypath);
         String key = Files.readString(fileName);
 
         JDA jda = JDABuilder.createDefault(key).build();
